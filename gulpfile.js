e041a11180c0f8dda53265fb754086ab70b23d45
cbare1208 <cliff.barendsen@vml.com>
2016-11-05 17:31:41 -0500

    added browser sync. removed sass. added stylus. stylus source mapping

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,88 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+
+var stylus = require('gulp-stylus');
+var sourcemaps = require('gulp-sourcemaps');
+
 var pug  = require('gulp-pug');
-
-var concat = require('gulp-concat');
-var minifyJs = require('gulp-minify');
 var htmlbeautify = require('gulp-html-beautify');
-var runsequence = require('gulp-run-sequence');
 var clean = require('gulp-clean');
+var browserSync = require('browser-sync').create();
 
-var srcAssetBase = 'src/assets/';
-var srcViewBase = 'src/views/';
-var srcScriptsBase = srcAssetBase + 'scripts/';
-var distAssetBase = 'build/assets';
+var srcBase = 'src/';
+var srcAssetBase = srcBase + 'assets/';
+var srcViewBase = srcBase + 'views/';
+var distBase = 'build/';
+var distAssetBase = distBase + 'assets';
 
-gulp.task('sass', function () {
-  return gulp.src(srcAssetBase + 'scss/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest(distAssetBase + '/css'))
+
+gulp.task('stylus', ['build-clean'], function (cb) {
+  return gulp.src(srcAssetBase + 'stylus/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest(distAssetBase + '/css'));
 });
 
-// gulp.task('concat-vendor', function() {
-//   var mainJs = srcScriptsBase + 'main.js';
-//   var vendorPath = srcScriptsBase + 'vendor/';
-//   var jQuery = vendorPath + 'jquery.js';
-//   var slick = vendorPath + 'slick.js';
+gulp.task('linenos', ['stylus'], function (cb) {
+  return gulp.src(srcAssetBase + 'stylus/*.styl')
+    .pipe(stylus({linenos: true}))
+    .pipe(gulp.dest(distAssetBase + '/css'));
+});
  
-//   return gulp.src([jQuery, slick, mainJs])
-//     .pipe(concat('compiled.js'))
-//     .pipe(gulp.dest(distAssetBase + '/scripts/'));
-// });
-
-gulp.task('minify', function() {
-  gulp.src(srcAssetBase + 'scripts/*.js')
-    .pipe(minifyJs({
-        ext:{
-            src:'-debug.js',
-            min:'.js'
-        },
-        exclude: ['tasks'],
-        ignoreFiles: ['.combo.js']
-    }))
-    .pipe(gulp.dest(distAssetBase + '/scripts'))
+gulp.task('sourcemaps-inline', ['linenos'], function () {
+  return gulp.src(srcAssetBase + 'stylus/*.styl')
+    .pipe(sourcemaps.init())
+    .pipe(stylus())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(distAssetBase + '/css'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
-
-
-gulp.task('pug', function () {
+gulp.task('pug', function (cb) {
   return gulp.src(srcViewBase + '*.pug')
     .pipe(pug())
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest(distBase));
 });
 
-gulp.task('htmlbeautify', function() {
+gulp.task('htmlbeautify', ['pug'], function() {
   var options = {indentSize: 2};
-  gulp.src('build/*.html')
+  gulp.src(distBase + '*.html')
     .pipe(htmlbeautify(options))
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest(distBase))
+    .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('scripts', function(cb) {
- runsequence('concat-vendor',cb);
+gulp.task('app-scripts', function(){
+  return gulp.src([srcAssetBase + 'js/**/*.js', '!' + srcAssetBase + 'js/vendors/*.js'])
+    .pipe(gulp.dest(distAssetBase + '/js'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('build', function(cb){
-  runsequence('build-clean', ['sass', 'pug'], 'htmlbeautify', cb);
+gulp.task('vendor-scripts', function(){
+  return gulp.src(srcAssetBase + 'js/vendors/**/*.js')
+    .pipe(gulp.dest(distAssetBase + '/js/vendors'));
 });
 
-gulp.task('build-clean', function(){
-  return gulp.src('build').pipe(clean());
-})
+gulp.task('build-scripts', ['vendor-scripts', 'app-scripts']);
+
+gulp.task('build-clean', function(cb){
+  return gulp.src('htdocs').pipe(clean());
+});
+
+gulp.task('build-stylus', ['stylus', 'linenos', 'sourcemaps-inline']);
+gulp.task('build-pug', ['pug', 'htmlbeautify']);
+
+gulp.task('build', ['build-clean', 'build-scripts', 'build-stylus', 'build-pug']);
+
+gulp.task('browser', function() {
+    browserSync.init({
+        port: 8080,
+        server: {
+            baseDir: "./build"
+        }
+    });
+});
+
+gulp.task('watch', ['browser'], function () {
+    gulp.watch("src/assets/js/**/*.js", ['app-scripts']);
+    gulp.watch("src/assets/stylus/**/*.styl", ['build-stylus']);
+    gulp.watch("src/**/*.pug", ['build-pug']);
+});
